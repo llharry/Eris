73da4fbd563ae67e029b83478f6516f7e44eaf96
llharry <llharry2@hotmail.co.uk>
2017-06-15 17:38:50 +0100

    Fin

--- page.php
+++ page.php
@@ -5,7 +5,7 @@ get_header(); ?>
 if (have_posts()) {
     while (have_posts()) {the_post();
 ?>
-<section class="content">
+<section class="content defaultPageContent">
     <div class="title">
         <h1><?php the_title() ?></h1>
     </div>
